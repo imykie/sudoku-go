3076952b670c4f05e105e8b10d34f67e0f7f12c1
Michael Olatunji <tundexmike@gmail.com>
2021-06-26 14:05:28 +0100

    sudoku solver

--- main.go
+++ main.go
@@ -13,6 +13,10 @@ type Pos struct {
 	x, y int
 }
 
+type Game struct {
+	board [N][N]int
+}
+
 func main() {
 	board := [N][N]int{
 		{3, 0, 6, 5, 0, 8, 4, 0, 0},
@@ -25,10 +29,74 @@ func main() {
 		{0, 0, 0, 0, 0, 0, 0, 7, 4},
 		{0, 0, 5, 2, 0, 6, 3, 0, 0},
 	}
-	printBoard(board)
+
+	//board2 := [N][N]int{
+	//{5, 3, 0, 0, 7, 0, 0, 0, 0},
+	//{6, 0, 0, 1, 9, 5, 0, 0, 0},
+	//{0, 9, 8, 0, 0, 0, 0, 6, 0},
+	//{8, 0, 0, 0, 6, 0, 0, 0, 3},
+	//{4, 0, 0, 8, 0, 3, 0, 0, 1},
+	//{7, 0, 0, 0, 2, 0, 0, 0, 6},
+	//{0, 6, 0, 0, 0, 0, 2, 8, 0},
+	//{0, 0, 0, 4, 1, 9, 0, 0, 5},
+	//{0, 0, 0, 0, 8, 0, 0, 7, 9},
+	//}
+
+	printBoard(&board)
+	solution, err := solve(&board)
+	if err == nil {
+		fmt.Println("Sudoku board solution 😎🚀🚀🚀")
+		printBoard(solution)
+	} else {
+		panic(err)
+	}
 }
 
-func valid(board [N][N]int, num int, p *Pos) bool {
+func solve(board *[N][N]int) (*[N][N]int, error) {
+	if isGameOver(board) {
+		return board, nil
+	}
+
+	cell, err := checkEmpty(board)
+	if err != nil {
+		return board, err
+	}
+
+	for val := 1; val < 10; val++ {
+		board[cell.x][cell.y] = val
+		if valid(board, val, cell) {
+			sol, err := solve(board)
+			if err == nil {
+				return sol, nil
+			}
+		}
+		board[cell.x][cell.y] = 0
+	}
+
+	return board, errors.New("Unable to solve game")
+}
+
+func solveBoard(board *[N][N]int) {
+	for row := 1; row <= 9; row++ {
+		for col := 1; col <= 9; col++ {
+			if board[row-1][col-1] == 0 {
+				for value := 1; value < 10; value++ {
+					cell := &Pos{row - 1, col - 1}
+					if valid(board, value, cell) {
+						board[cell.x][cell.y] = value
+						solveBoard(board)
+						board[cell.x][cell.y] = 0
+					}
+				}
+				return
+			}
+		}
+	}
+	printBoard(board)
+	return
+}
+
+func valid(board *[N][N]int, num int, p *Pos) bool {
 	boxI := p.x / 3
 	boxJ := p.y / 3
 
@@ -58,21 +126,32 @@ func valid(board [N][N]int, num int, p *Pos) bool {
 	return true
 }
 
-func checkEmpty(board [N][N]int) (*Pos, error) {
-	for i, _ := range board {
-		for j, _ := range board[i] {
-			if board[i][j] == 0 {
-				return &Pos {i, j}, nil
+func checkEmpty(board *[N][N]int) (*Pos, error) {
+	for i, row := range board {
+		for j, val := range row {
+			if val == 0 {
+				return &Pos{i, j}, nil
 			}
 		}
 	}
-	return nil, errors.New("No empty field")
+	return nil, errors.New("no empty case found")
 }
 
-func printBoard(board [N][N]int) {
+func isGameOver(board *[N][N]int) bool {
+	for _, line := range board {
+		for _, val := range line {
+			if val == 0 {
+				return false
+			}
+		}
+	}
+	return true
+}
+
+func printBoard(board *[N][N]int) {
 	for i, _ := range board {
 		if i%3 == 0 && i != 0 {
-			fmt.Println(" _ _ _ _ _ _ _ _ _ _ _ _ _ _ _ _")
+			fmt.Println(" - - - - - - - - - - - - - - - -")
 		}
 		for j, _ := range board[i] {
 			if j%3 == 0 && j != 0 {
@@ -85,4 +164,5 @@ func printBoard(board [N][N]int) {
 			}
 		}
 	}
+	fmt.Printf("\n\n")
 }
