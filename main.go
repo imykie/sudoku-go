272563b5d1c93bfcfc19415b4bad02c6cc542e07
Michael Olatunji <tundexmike@gmail.com>
2021-06-26 01:56:46 +0100

    adding checkEmpty and valid methods

--- main.go
+++ main.go
@@ -1,10 +1,17 @@
 package main
 
-import "fmt"
+import (
+	"errors"
+	"fmt"
+)
 
 const (
 	N = 9
 )
+
+type Pos struct {
+	x, y int
+}
 
 func main() {
 	board := [N][N]int{
@@ -21,13 +28,54 @@ func main() {
 	printBoard(board)
 }
 
+func valid(board [N][N]int, num int, p *Pos) bool {
+	boxI := p.x / 3
+	boxJ := p.y / 3
+
+	// check row
+	for i, _ := range board {
+		if board[p.x][i] == num && p.y != i {
+			return false
+		}
+	}
+
+	// check col
+	for i, _ := range board {
+		if board[i][p.y] == num && p.x != i {
+			return false
+		}
+	}
+
+	// check box
+	for i := boxI * 3; i < boxI+3; i++ {
+		for j := boxJ * 3; j < boxJ+3; j++ {
+			if board[i][j] == num && p.x != i && p.y != j {
+				return false
+			}
+		}
+	}
+
+	return true
+}
+
+func checkEmpty(board [N][N]int) (*Pos, error) {
+	for i, _ := range board {
+		for j, _ := range board[i] {
+			if board[i][j] == 0 {
+				return &Pos {i, j}, nil
+			}
+		}
+	}
+	return nil, errors.New("No empty field")
+}
+
 func printBoard(board [N][N]int) {
 	for i, _ := range board {
-		if i % 3 == 0 && i != 0 {
+		if i%3 == 0 && i != 0 {
 			fmt.Println(" _ _ _ _ _ _ _ _ _ _ _ _ _ _ _ _")
 		}
 		for j, _ := range board[i] {
-			if j % 3 == 0 && j != 0 {
+			if j%3 == 0 && j != 0 {
 				fmt.Printf(" | ")
 			}
 			if j == 8 {
